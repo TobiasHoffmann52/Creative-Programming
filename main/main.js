2a0dcb644de9b0972d97a84b851c9d70edaccc28
TobiasHoffmann52 <88829745+TobiasHoffmann52@users.noreply.github.com>
2026-04-13 17:38:57 +0200

    update 1

--- main/main.js
+++ main/main.js
@@ -1,10 +1,21 @@
 let score = 0;
 let trees = [];
 
-function setup() {
-  createCanvas(1200, 800);
-  noStroke();
+let textArray = [];
+let showGenerating = false;
+let lastDotFrame=0;
+let startDotFrame=0;
+let circle_x=150;
+let circle_y=400;
+let circle_diam=200;
 
+let textX = 100;
+let textY = 400;
+
+function setup() {
+  createCanvas(1500, 800);
+  frameRate(60);
+  noStroke();
 }
 
 
@@ -24,12 +35,52 @@ function draw() {
   }
 
   calculateScore();
-  
-  for (let i = 0; i <= 5; i++) {
-    let x = random (0, width);
-    let y = random (0, height/3);
-    fill(255, 180);
-    ellipse(x, y, 150, 70);
+
+  let glow = 255;
+  if (showGenerating) { // hvis "enter" bliver trykket på vil cirklen begynde at "glow"
+    glow = 100 + 40 * sin(frameCount * 0.03); // Sin is used for pulsing - glow er et tal imellem 60 og 140
+  }
+  //circle
+  noStroke();
+  fill(128, 0, 128, glow);
+
+  if (showGenerating) {
+    fillGradient('radial', {
+    from :
+      [circle_x, circle_y, 0], // x, y, radius
+      to :
+      [circle_x, circle_y, circle_diam], // x, y, radius
+      steps :
+      [
+        color(128 + 100 * sin(frameCount * 0.03), 0, 128 + 100 * sin(frameCount * 0.03)),
+        color(128 + 50 * sin(frameCount * 0.03), 0, 128 + 50 * sin(frameCount * 0.03)),
+        color(128 + 10 * sin(frameCount * 0.03), 0, 128 + 10 * sin(frameCount * 0.03)),
+        color(128, 0, 128),
+        //          color(0, 96, 164),
+        //          color()
+        ] // Array of p5.color objects or arrays containing [p5.color Object, Color Stop (0 to 1)]
+    }
+    );
+  }
+  circle(circle_x, circle_y, circle_diam, glow);
+
+
+  // searchbar rect
+  stroke(128, 0, 128, 255-glow);
+  fill (253);
+  stroke(0, 0, 0);
+  rect (50, 550, 200, 50, 10);
+  let combinedText = join(textArray, '');
+  fill (0);
+
+  // "!" betyder "ikke/not"
+  if (!showGenerating) {
+  }
+  textSize(15);
+  noStroke();
+  text(combinedText, 60, 575);
+  if (showGenerating) {
+    generating();
   }
 }
 
@@ -40,6 +91,25 @@ function mouseClicked() {
   }
 }
 
+function keyPressed() {
+
+  if (keyCode == 8) { //length er længden på arrayet, ved at -1, kan man slette det sidste index i sin tekst
+    textArray.splice(textArray.length-1, 1);
+  }
+
+  // "eller" er "||" i en if statement
+  if (keyCode <= 90 && keyCode >= 65 || keyCode == 32) {
+    if (textArray.length < 30) { // begrænsning på hvormange bogstaver der kan skrives
+      textArray.push(key);
+    }
+  }
+  if (keyCode == 13) {
+    showGenerating = true;
+    startDotFrame = frameCount;
+  }
+  //console.log(textArray.length);
+} // keyPressed()
+
 
 function writeScore(x, y) {
   // Writes the current CO2 emissions on the specified x/y-coordinates
@@ -49,7 +119,7 @@ function writeScore(x, y) {
   textAlign(LEFT, CENTER);
   textSize(36);
   fill(255);
-  text("CO2 emissions: " + score + "g", x, y);
+  text("CO2 emissions: " + int(score) + "g", x, y);
 
   pop();
 }
@@ -57,4 +127,34 @@ function writeScore(x, y) {
 
 function calculateScore() {
   score = 0 - (trees.length * 10);
+  score = score + frameCount/50;
+}
+
+function generating() {
+  //let time = Date.now();
+  // let glow = 100 + 40 * sin(frameCount * 0.90); // Sin is used for pulsing
+  console.log("Generating...");
+  textSize(20);
+  fill(250, 120);
+
+  //  let dots = frameCount
+  let framesPassed = frameCount - startDotFrame;
+  if (framesPassed < 25) {
+    text("Generating", textX, textY);
+    console.log("framesPassed :", framesPassed);
+  }
+  if (framesPassed > 25 && framesPassed < 50) {
+    text("Generating.", textX, textY);
+    console.log("framesPassed :", framesPassed);
+  }
+  if (framesPassed > 50 && framesPassed < 75) {
+    text("Generating..", textX, textY);
+    console.log("framesPassed :", framesPassed);
+  }
+  if (framesPassed > 75 && framesPassed < 100) {
+    text("Generating...", textX, textY);
+    console.log("framesPassed :", framesPassed);
+    startDotFrame = frameCount;
+  }
+  console.log("frameCount :", frameCount);
 }
